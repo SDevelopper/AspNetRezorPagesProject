11a5d81d5ab70c2ccf144c0272ea4fc34e9b15cb
SDevelopper <ns3954881@gmail.com>
2026-02-15 22:53:27 +0400

    Initial project setup: added login and registration structure, DTO models, DbContext, database connection, and cookie authentication configuration

---
+++ AspNetRezorPagesProject/Models/Entity/User.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetRezorPagesProject.Models.Entity
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string HashPassword { get; set; } = string.Empty;
+    }
+}
---
+++ AspNetRezorPagesProject/Program.cs
@@ -0,0 +1,58 @@
+using AspNetRezorPagesProject.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetRezorPagesProject
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddAuthentication("Cookies")
+                .AddCookie("Cookies", options =>
+                {
+                    options.LoginPath ="/Auth/Login";
+                    options.LogoutPath = "/Auth/Logout";
+
+                    options.ExpireTimeSpan=TimeSpan.FromDays(7);
+                    options.SlidingExpiration=true;
+
+                    options.Cookie.HttpOnly = true;
+                    options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+                    options.Cookie.SameSite=SameSiteMode.Strict;
+                });
+            builder.Services.AddAuthorization();
+                
+
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapStaticAssets();
+            app.MapRazorPages()
+               .WithStaticAssets();
+
+            app.Run();
+        }
+    }
+}
